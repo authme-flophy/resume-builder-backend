0052192d32d12f73691e1a7cdaba1b2380e4d752
waikau maina <ericmaina.dev@gmail.com>
2022-12-27 12:05:26 +0300

    removed the Resume resouce

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :schools
   resources :positions
   resources :programming_languages
-  resources :resumes
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
