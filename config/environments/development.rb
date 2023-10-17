1e71d67e627931886e64c98e66d6d3a53f180a52
waikau maina <ericmaina.dev@gmail.com>
2023-10-17 13:47:49 +0300

    fixed the hosts allowed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "resume-builder-production.up.railway.app"
+  config.hosts << "resume-builder-v7jb.onrender.com"
 
 
   # In the development environment your application's code is reloaded any time
